43cd78977dbb1d90b35dfa32212f746c34a9a7a9
wermut <wermut4869@gmail.com>
2024-08-27 15:36:36 +0900

    파일연습완료

---
+++ demoFile.py
@@ -0,0 +1,21 @@
+# DemoFile.py
+
+#쓰기
+
+f = open("c:\\work\\text.txt","wt",encoding="utf-8")
+f.write("첫번째라인\n두번째라인\n세번째라인\n")
+
+f.close()
+
+#읽기
+
+f= open(r"c:\work\text.txt","rt",encoding="utf-8")
+result = f.read()
+
+
+#print(result)
+print(f.readline())
+f.close()
+
+
+
